7624b3322df6cfa18f7c9b8458856d9adf41a874
Cherry <ilovecherries@protonmail.com>
2021-08-20 19:44:32 -0400

    uhhh it should update regardless of version now? it SHOULD be good mmm

--- src/main/java/net/fabricmc/ilovecherries/GCPLoader.java
+++ src/main/java/net/fabricmc/ilovecherries/GCPLoader.java
@@ -15,6 +15,8 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
+import java.util.Objects;
+import java.util.stream.Stream;
 
 public class GCPLoader implements ModInitializer {
 	private static final String API_FORMAT = "https://api.github.com/repos/%s/%s";
@@ -22,7 +24,27 @@ public class GCPLoader implements ModInitializer {
 			.getInstance()
 			.runDirectory
 			.getAbsolutePath()
-			+ "/mods/%s";
+			+ "/mods/";
+	private static final String EXPANDED_NAME_REGEX = "-[0-9].+\\.jar";
+	private static boolean DEV_MODE = false;
+	private static String SELF_NAME = "github-custom-pack-loader";
+
+	private String trimJarName(String name) {
+		return name.replaceAll(EXPANDED_NAME_REGEX, "");
+	}
+
+	private File expandJarName(String filename) {
+		File folder = new File(MOD_FOLDER);
+
+		if (DEV_MODE && filename.equals(SELF_NAME)) {
+			return null;
+		}
+
+		return Arrays.stream(folder.listFiles())
+				.filter(x -> x.getName().matches(filename + EXPANDED_NAME_REGEX))
+				.findFirst()
+				.orElse(null);
+	}
 
 	private void parseGithubResponse(String data) {
 		Gson gson = new Gson();
@@ -31,33 +53,26 @@ public class GCPLoader implements ModInitializer {
 
 		// first, we should go through the cache that we have saved on our system and see if
 		// some of the mods need to be removed
-		File cacheFile = new File(String.format(MOD_FOLDER, "custompackcache.json"));
+		File cacheFile = new File(MOD_FOLDER + "custompackcache.json");
+
 		if (cacheFile.exists() && !cacheFile.isDirectory()) {
 			try {
 				String cachedData = FileUtils.readFileToString(cacheFile, StandardCharsets.UTF_8);
 				FileMetadata[] cachedFileArray = gson.fromJson(cachedData, FileMetadata[].class);
 
-				for (FileMetadata cachedFileMetadata : cachedFileArray) {
-					if (Arrays.stream(fileArray).noneMatch(x -> cachedFileMetadata.name.equals(x.name))) {
-						File cachedFile = new File(String.format(MOD_FOLDER, cachedFileMetadata.name));
-
-						System.out.println("Deleting: " + cachedFileMetadata.name);
-
-						if (cachedFile.delete()) {
-							System.out.println("Successfully deleted: " + cachedFileMetadata.name);
-							GCPState.addDeleted(cachedFileMetadata.name);
-						} else {
-							System.out.println("Unable to delete: " + cachedFileMetadata.name);
-						}
-					}
-				}
+				Arrays.stream(cachedFileArray)
+					.filter(x -> Arrays.stream(fileArray)
+						.noneMatch(y -> trimJarName(x.name).equals(trimJarName(y.name))))
+					.map(x -> expandJarName(x.name))
+					.filter(Objects::nonNull)
+					.filter(File::delete)
+					.forEach(x -> GCPState.addDeleted(x.getName()));
 
 				try {
 					FileUtils.writeStringToFile(cacheFile, data, StandardCharsets.UTF_8);
 				} catch (IOException e) {
 					System.err.println(e);
 				}
-
 			} catch (IOException e) {
 				System.err.println(e);
 			}
@@ -70,26 +85,31 @@ public class GCPLoader implements ModInitializer {
 		}
 
 		for (FileMetadata githubFile : fileArray) {
-			final String fileLocation = String.format(MOD_FOLDER, githubFile.name);
-			File file = new File(fileLocation);
+			File file = expandJarName(trimJarName(githubFile.name));
+			file = file == null
+					? new File(MOD_FOLDER + githubFile.name)
+					: file;
 
 			// check if the file exists first I suppose
 			if (file.exists() && !file.isDirectory()) {
 				// check if the SHA-256 hash matches
 				try (InputStream is = new FileInputStream(file)) {
 					String hash = DigestUtils.sha256Hex(is);
-					if (hash.equals(githubFile.sha)) {
-						System.out.println("Updating: " + githubFile.name);
+					if (!(DEV_MODE && trimJarName(githubFile.name).equals(SELF_NAME))
+						&& hash.equals(githubFile.sha)) {
+						System.out.println("Updating " + file.getName() + " to " + githubFile.name);
 						URL url = new URL(githubFile.download_url);
-						FileUtils.copyURLToFile(url, file);
-						System.out.println("Updated: " + githubFile.name);
+						file.delete();
+						File newFile = new File(MOD_FOLDER + githubFile.name);
+						FileUtils.copyURLToFile(url, newFile);
+						System.out.println("Updated: " + trimJarName(githubFile.name));
 						GCPState.addUpdated(githubFile.name);
 					}
 				} catch (IOException e) {
 					System.err.println(e);
 				}
 			}
-			else {
+			else if (!(DEV_MODE && trimJarName(githubFile.name).equals(SELF_NAME))){
 				try {
 					System.out.println("Downloading " + githubFile.name);
 					URL url = new URL(githubFile.download_url);
@@ -109,7 +129,7 @@ public class GCPLoader implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		File configFile = new File(String.format(MOD_FOLDER, "custompackconfig.json"));
+		File configFile = new File(MOD_FOLDER + "custompackconfig.json");
 
 		if (configFile.exists() && !configFile.isDirectory()) {
 			try {
@@ -120,7 +140,7 @@ public class GCPLoader implements ModInitializer {
 				HttpClient client = HttpClient.newHttpClient();
 
 				HttpRequest request = HttpRequest.newBuilder()
-						.uri(URI.create(String.format(API_FORMAT, "ilovecherries/test-load-repo", "contents/")))
+						.uri(URI.create(String.format(API_FORMAT, config.getRepoURL(), "contents/")))
 						.build();
 
 				client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
